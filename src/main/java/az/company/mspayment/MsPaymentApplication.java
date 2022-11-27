27efe565c6e537949272c985511249a8118a6518
huseynahmadov <t.huseyn96@mail.ru>
2022-11-27 21:43:55 +0400

    done

--- src/main/java/az/company/mspayment/MsPaymentApplication.java
+++ src/main/java/az/company/mspayment/MsPaymentApplication.java
@@ -2,8 +2,12 @@ package az.company.mspayment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication(scanBasePackages = "az.company.mspayment.client")
+@SpringBootApplication
+@EnableFeignClients
 public class MsPaymentApplication {
 
     public static void main(String[] args) {
